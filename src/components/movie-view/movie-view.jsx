d113d8a48807956c61d7d108ae5317f176034b83
DK-CF <korotkovadvisory@gmail.com>
2025-10-08 11:25:45 +0200

    A3-E3.4 Finalized all views. Added detailed PropTypes.

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -5,40 +5,39 @@
  */
 
 // --- Core Node.js modules (none used here) ---
-
 // --- React and other Third-party libraries (none used here) ---
 import PropTypes from "prop-types";
-
 // --- Local application imports (none required) ---
 
+// Movie view component
 export const MovieView = ({ movie, onBackClick }) => {
     return (
-      <div>
         <div>
-          <img src={movie.image_url} />
+            <div>
+            <img src={movie.image_url} />
+            </div>
+            <div>
+            <span>Title: </span>
+            <span>{movie.title}</span>
+            </div>
+            <div>
+            <span>Description: </span>
+            <span>{movie.description}</span>
+            </div>
+            <div>
+            <span>Director: </span>
+            <span>{movie.director.name}</span>
+            </div>
+            <div>
+            <span>Genre:</span>
+            <span>{movie.genre.name}</span>
+            </div>
+            <div>
+            <span>Release year: </span>
+            <span>{movie.release_year}</span>
+            </div>
+            <button onClick={onBackClick}>Back</button>
         </div>
-        <div>
-          <span>Title: </span>
-          <span>{movie.title}</span>
-        </div>
-        <div>
-          <span>Description: </span>
-          <span>{movie.description}</span>
-        </div>
-        <div>
-          <span>Director: </span>
-          <span>{movie.director.name}</span>
-        </div>
-        <div>
-          <span>Genre:</span>
-          <span>{movie.genre.name}</span>
-        </div>
-        <div>
-          <span>Release year: </span>
-          <span>{movie.release_year}</span>
-        </div>
-        <button onClick={onBackClick}>Back</button>
-      </div>
     );
   };
 
@@ -46,14 +45,21 @@ MovieView.propTypes = {
     movie: PropTypes.shape({
         title: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
+        release_year: PropTypes.string.isRequired,
         image_url: PropTypes.string.isRequired,
+        rating_imdb: PropTypes.string,
+        featured: PropTypes.bool,
+        starring: PropTypes.arrayOf(PropTypes.string),
         director: PropTypes.shape({
             name: PropTypes.string.isRequired,
             bio: PropTypes.string,
             birth_date: PropTypes.string,
             death_date: PropTypes.string
         }).isRequired,
-        release_year: PropTypes.string.isRequired
-    }),
+        genre: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            description: PropTypes.string
+        }).isRequired,
+    }).isRequired,
     onBackClick: PropTypes.func.isRequired
 }; 
