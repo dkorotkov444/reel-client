31191cb1b7e1d83cdee31316d87a95d21a0e40d6
DK-CF <korotkovadvisory@gmail.com>
2025-10-14 13:21:15 +0200

    A3-E3.6 Changed to Bootstrap form in signup view.

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -9,6 +9,9 @@
 // --- Core Node.js modules (none used here) ---
 // --- React and other Third-party libraries ---
 import { useState } from "react";
+// Import React Bootstrap components
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 // --- Local application imports ---
 
 // Signup view component
@@ -50,55 +53,56 @@ export const SignupView = ({ onLoggedIn }) => {
 
     // Rendering the signup form
     return (
-        <form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit}>
 
-        <label>
-            Username:
-            <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-            minLength="3"
-            autoComplete="off"
-            />
-        </label>
+            <Form.Group controlId="formUsername">
+                <Form.Label>Username: </Form.Label>
+                <Form.Control
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    minLength="3"
+                    autoComplete="off"
+                />
+            </Form.Group>
 
-        <label>
-            Password:
-            <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            minLength="8"
-            autoComplete="new-password"
-            />
-        </label>
-        
-        <label>
-            Email:
-            <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            autoComplete="off"
-            />
-        </label>
+            <Form.Group controlId="formPassword">
+                <Form.Label>Password: </Form.Label>
+                <Form.Control
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    minLength="8"
+                    autoComplete="new-password"
+                />
+            </Form.Group>
 
-        <label>
-            Birth date:
-            <input
-            type="date"
-            value={birth_date}
-            onChange={(e) => setBirthDate(e.target.value)}
-            required
-            autoComplete="off"
-            />
-        </label>
+            <Form.Group controlId="formEmail">
+                <Form.Label>Email: </Form.Label>
+                <Form.Control
+                    type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    autoComplete="off"
+                />
+            </Form.Group>
 
-        <button type="submit">Submit</button>
-        </form>
+            <Form.Group controlId="formBirthDate">
+                <Form.Label>Birth date: </Form.Label>
+                <Form.Control
+                    type="date"
+                    value={birth_date}
+                    onChange={(e) => setBirthDate(e.target.value)}
+                    required
+                    autoComplete="off"
+                />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">Submit</Button>
+
+        </Form>
     );
 };
